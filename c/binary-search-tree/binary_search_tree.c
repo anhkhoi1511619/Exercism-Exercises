7af4761404215db3ccb1dc19f781c65a13e58a83
Khoi15111619 <anhkhoi1511619@gmail.com>
2026-02-28 10:48:01 +0900

    Add result of binary_search

--- c/binary-search-tree/binary_search_tree.c
+++ c/binary-search-tree/binary_search_tree.c
@@ -1,42 +1,46 @@
 #include "binary_search_tree.h"
-// static void traverse(node_t *node);
+
+static node_t *make_node(int value) {
+    node_t *n = malloc(sizeof(node_t));
+    // if (!n) return NULL;
+    n->data = value;
+    n->left = NULL;
+    n->right = NULL;
+    printf("make node\n");
+    printf("nt->data pos1: %d\n", n->data);
+    printf("nt->right: %d, nt->right->data = %d\n", n->right == NULL, (n->right == NULL) ? 0 : n->right->data);
+    printf("nt->left: %d, nt->left->data = %d\n", n->left == NULL, (n->left == NULL) ? 0 : n->left->data);
+    return n;
+}
+
+static node_t *create_tree(node_t *root, int value) {
+    if (root == NULL) {
+        printf("root NULL\n");
+        return make_node(value);
+    }
+    printf("nt->data pos2: %d\n", root->data);
+    if (value <= root->data) {
+        root->left = create_tree(root->left, value);
+    } else if (value > root->data) {
+        root->right = create_tree(root->right, value);
+    }
+    printf("nt->data pos3: %d\n", root->data);
+    printf("nt->right: %d, nt->right->data = %d\n", root->right == NULL, (root->right == NULL) ? 0 : root->right->data);
+    printf("nt->left: %d, nt->left->data = %d\n", root->left == NULL, (root->left == NULL) ? 0 : root->left->data);
+    return root;
+}
 
 node_t *build_tree(int *tree_data, size_t tree_data_len) {
-    node_t *nt = malloc(sizeof(int) + tree_data_len * sizeof(int));
-    printf("size %zu\n", tree_data_len);
-    nt->data = *tree_data++;
-    printf("nt->data %d\n", nt->data);
-    for (size_t i = 1; i < tree_data_len; i++) {
-        // printf("data = %d i = %zu\n", *tree_data, i);
-        int data = *tree_data++;
-        if (data > nt->data) {
-            printf("right tmp_nt->data: %d > nt->data: %d\n", data, nt->data);
-            nt->right = malloc(sizeof(int));
-            nt->right->data = data;
-        } else {
-            printf("left tmp_nt->data: %d <= nt->data: %d\n", data, nt->data);
-            nt->left = malloc(sizeof(int));
-            nt->left->data = data;
-        }
-        printf("nt->right: %d, nt->right->data = %d\n", nt->right == NULL, (nt->right == NULL) ? 0 : nt->right->data);
-        printf("nt->left: %d, nt->left->data = %d\n", nt->left == NULL, (nt->left == NULL) ? 0 : nt->left->data);
-        // tmp_nt = realloc(tmp_nt, sizeof(int));
+    node_t *nt = malloc(sizeof(int) + sizeof(*nt));
+    nt->data = *tree_data;
+    nt->left = nt->right = NULL;
+    for (size_t i = 1; i < tree_data_len; i++)
+    {
+        nt = create_tree(nt, tree_data[i]);
     }
-    // traverse(nt);
-    // for(size_t i = 0; i < tree_data_len; i++) {
-    //     printf("data %d\n", tree_data[i]);
-    //     if ()
-    // }
     return nt;
 }
-// static void traverse(node_t *node)
-// {
-//     if (node == NULL) return;
-//     printf("%d\n", node->data);
 
-//     traverse(node->left);
-//     traverse(node->right);
-// }
 void free_tree(node_t *tree) {
     free(tree);
 }
